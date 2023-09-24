862a8aa7adae306d87761b48b668411d6b627eef
Adam Kulesza <adam.kule@gmail.com>
2023-09-24 19:30:13 +0200

    Adds proc_stat_cpu_stats struct

--- include/top_adam/proc_stat.h
+++ include/top_adam/proc_stat.h
@@ -1,5 +1,20 @@
 #ifndef __PROC_STAT_H
 #define __PROC_STAT_H
 
+typedef struct proc_stat_cpu_info proc_stat_cpu_info;
+struct proc_stat_cpu_info {
+	int cpu_id;
+
+	unsigned long long int usertime;
+	unsigned long long int nicetime;
+	unsigned long long int systemtime;
+	unsigned long long int idletime;
+	unsigned long long int iowait;
+	unsigned long long int irq;
+	unsigned long long int softirq;
+	unsigned long long int steal;
+	unsigned long long int guest;
+	unsigned long long int guest_nice;
+};
 
 #endif
