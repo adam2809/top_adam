c83ee8579d5c11f42e11a54254ba1c8aa7dd1bb7
Adam Kulesza <adam.kule@gmail.com>
2023-09-26 21:49:28 +0200

    Implements logger

---
+++ include/top_adam/ta_logger.h
@@ -0,0 +1,21 @@
+#ifndef __TA_LOGGER_H
+#define __TA_LOGGER_H
+
+#include <time.h>
+
+#define MAX_MSG_LEN 150
+
+typedef struct ta_logger_msg{
+	time_t time;
+	char msg[MAX_MSG_LEN];
+} ta_logger_msg;
+#undef MAX_MSG_LEN
+
+ta_logger_msg* ta_logger_msg_new();
+int ta_logger_init();
+int ta_logger_core(void* arg);
+void ta_log(char* msg);
+void ta_logger_destroy();
+
+
+#endif
