e366405301fdfc27135e199bf09e8f0c55f4bfca
Adam Kulesza <adam.kule@gmail.com>
2023-09-27 22:52:08 +0200

    Adds queues
    for q

--- src/main.c
+++ src/main.c
@@ -1,3 +1,18 @@
+#include "ta_queue.h"
+
+int reader_thread(void* arg);
+int analyzer_thread(void* arg);
+int printer_thread(void* arg);
+
+ta_queue* cpu_info_queue;
+ta_queue* prev_cpu_info_queue;
+
 int main(int argc, char **argv) {
+	cpu_info_queue = ta_queue_new();
+	prev_cpu_info_queue = ta_queue_new();
+
+	if(!cpu_info_queue || !prev_cpu_info_queue){
+		return 1;
+	}
 	return 0;
 }
