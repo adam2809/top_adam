9edd7283082c353124cce54e1a52890073616a12
Adam Kulesza <adam.kule@gmail.com>
2023-10-02 01:39:20 +0200

    Adds thread safe append

--- include/top_adam/ta_safe_queue.c
+++ include/top_adam/ta_safe_queue.c
@@ -1,1 +1,21 @@
 #include "ta_safe_queue.h"
+
+void* ta_queue_safe_append(ta_queue* queue, void* val , mtx_t* mtx, cnd_t* cnd_full, cnd_t* cnd_empty){
+	void* ret;
+
+	mtx_lock(mtx);
+
+	if (ta_queue_is_full(queue))
+	{
+		cnd_wait(cnd_full,mtx);
+	}
+
+	ret = ta_queue_append(queue, val);
+	if(ret){
+		cnd_signal(cnd_empty);
+	}
+
+	mtx_unlock(mtx);
+	return ret;
+	
+}
--- include/top_adam/ta_safe_queue.h
+++ include/top_adam/ta_safe_queue.h
@@ -1,3 +1,4 @@
 #ifndef __TA_SAFE_QUEUE_H
 #define __TA_SAFE_QUEUE_H
+void* ta_queue_safe_append(ta_queue* queue, void* val , mtx_t* mtx, cnd_t* cnd_full, cnd_t* cnd_empty);
 #endif
