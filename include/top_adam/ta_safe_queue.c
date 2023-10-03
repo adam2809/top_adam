38a7e9782864add32cd697973b1b3a5dee19ad9c
Adam Kulesza <adam.kule@gmail.com>
2023-10-04 00:56:14 +0200

    Adds error handling to safe queue functions

--- include/top_adam/ta_safe_queue.c
+++ include/top_adam/ta_safe_queue.c
@@ -3,19 +3,19 @@
 void* ta_queue_safe_append(ta_queue* queue, void* val , mtx_t* mtx, cnd_t* cnd_full, cnd_t* cnd_empty){
 	void* ret;
 
-	mtx_lock(mtx);
+	if(mtx_lock(mtx) != thrd_success) return 0;
 
 	if (ta_queue_is_full(queue))
 	{
-		cnd_wait(cnd_full,mtx);
+		if(cnd_wait(cnd_full,mtx) != thrd_success) return 0;
 	}
 
 	ret = ta_queue_append(queue, val);
 	if(ret){
-		cnd_signal(cnd_empty);
+		if(cnd_signal(cnd_empty) != thrd_success) return 0;
 	}
 
-	mtx_unlock(mtx);
+	if(mtx_unlock(mtx) != thrd_success) return 0;
 	return ret;
 	
 }
@@ -23,15 +23,15 @@ void* ta_queue_safe_append(ta_queue* queue, void* val , mtx_t* mtx, cnd_t* cnd_f
 void* ta_queue_safe_pop(ta_queue* queue, mtx_t* mtx, cnd_t* cnd_full, cnd_t* cnd_empty){
 	void* ret;
 
-	mtx_lock(mtx);
+	if(mtx_lock(mtx) != thrd_success) return 0;
 
 	if(ta_queue_is_empty(queue)){
-		cnd_wait(cnd_empty,mtx);
+		if(cnd_wait(cnd_empty,mtx) != thrd_success) return 0;
 	}
 	ret = ta_queue_pop(queue);
-	cnd_signal(cnd_full);
+	if(cnd_signal(cnd_full) != thrd_success) return 0;
 
-	mtx_unlock(mtx);
+	if(mtx_unlock(mtx) != thrd_success) return 0;
 
 	return ret;
 }
